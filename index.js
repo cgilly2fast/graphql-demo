6a3be367411320d49897ec1060b04ac6fea0b1c7
Colby Gilbert <colbygilbert95@gmail.com>
2024-07-16 21:30:31 -0700

    feat: demo GraphQL pumbling

---
+++ index.js
@@ -0,0 +1,91 @@
+import { ApolloServer } from '@apollo/server'
+import { startStandaloneServer } from '@apollo/server/standalone'
+import db from './_db.js'
+import { typeDefs } from './schema.js'
+
+const resolvers = {
+	Query: {
+		games() {
+			return db.games
+		},
+		game(_, args) {
+			return db.games.find((game) => args.id === game.id)
+		},
+		reviews() {
+			return db.reviews
+		},
+		review(_, args) {
+			return db.reviews.find((review) => args.id === review.id)
+		},
+		authors() {
+			return db.authors
+		},
+		author(_, args) {
+			return db.authors.find((author) => args.id === author.id)
+		}
+	},
+	Game: {
+		reviews(parent) {
+			return db.reviews.filter((review) => review.game_id === parent.id)
+		}
+	},
+	Author: {
+		reviews(parent) {
+			return db.reviews.filter((review) => review.author_id === parent.id)
+		},
+	},
+	Review: {
+		author(parent) {
+			return db.authors.find((author) => author.id === parent.author_id)
+		},
+		game(parent) {
+			return db.games.find((game) => game.id === parent.game_id)
+		}
+	},
+	Mutation: {
+		deleteGame(_, args) {
+			db.games = db.games.filter((game) => game.id !== args.id)
+			return db.games
+		},
+		addGame(_, args) {
+			let game = {
+				...args.game,
+				id: Math.floor(Math.random() * 1000).toString()
+			}
+			db.games.push(game)
+			return game
+		},
+		updateGame(_, args) {
+			let editedGame = {}
+			let updatedList = []
+			for (let i = 0; i < db.games.length; i++) {
+				const game = db.games[i]
+				if (game.id === args.id) {
+					editedGame = { ...game, ...args.edits }
+					updatedList.push(editedGame)
+				} else {
+
+					updatedList.push(game)
+				}
+			}
+			db.games = updatedList
+			return editedGame
+
+		}
+	}
+}
+//server setup
+const server = new ApolloServer({
+	typeDefs,
+	resolvers,
+})
+
+const { url } = await startStandaloneServer(server, {
+
+	listen: { port: 4000 }
+})
+
+console.log('Server ready at: ', url)
+
+
+
